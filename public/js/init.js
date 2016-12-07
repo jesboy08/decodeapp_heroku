f16460b5fe3d7dd8613087fdbab680a820a094b7
jesboy06 <jessie.kun8@gmail.com>
2016-12-07 18:00:32 +0800

    mycommit

---
+++ public/js/init.js
@@ -0,0 +1,10 @@
+ $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+
+$('.carousel.carousel-slider').carousel({full_width: true});
+
+ $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+ 
